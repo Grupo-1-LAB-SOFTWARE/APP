3839e4c1c1d4736a523ca79abaaa8d1788c73da2
marksv21 <marvmms@gmail.com>
2024-01-20 13:28:03 -0300

    feat: gestão component
    - pagina de gestão para documentação do hadoc

---
+++ src/app/pages/gestao/gestao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GestaoComponent } from './gestao.component';
+
+
+@NgModule({
+  declarations: [
+    GestaoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GestaoModule { }
