6edc83e23070c1b84de1623e44e98d932fb35a5f
marksv21 <marvmms@gmail.com>
2024-02-07 20:59:59 -0300

    add: interceptor nos headers da requisição

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ComponentsModule } from './pages/components/components.module';
 import { CoreModule } from './core/core.module';
 
@@ -16,13 +16,16 @@ import { registerLocaleData } from '@angular/common';
 import localePT from '@angular/common/locales/pt';
 import { LoginComponent } from './pages/login/login.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { AutenticacaoInterceptor } from './core/interceptors/autenticacao.interceptor';
 
 registerLocaleData(localePT);
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    
+    CadastroComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -35,7 +38,12 @@ registerLocaleData(localePT);
     MaterialModule
   ],
   bootstrap: [AppComponent],
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-br' }],
+  providers: [{
+    provide: [HTTP_INTERCEPTORS,LOCALE_ID],
+    useClass: AutenticacaoInterceptor,
+    multi: true,
+    useValue: 'pt-br'
+  }],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class AppModule { }
