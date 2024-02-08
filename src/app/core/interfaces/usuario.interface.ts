273f4b1c87e240b658bcacbacb3bbe86e713797c
marksv21 <marvmms@gmail.com>
2024-02-07 21:00:33 -0300

    add: interface usuario

---
+++ src/app/core/interfaces/usuario.interface.ts
@@ -0,0 +1,12 @@
+// usuario.model.ts
+export interface Usuario {
+  id: number;
+  nome: string;
+  campos: string;
+  siape: string;
+  vinculo: string;
+  regimeTrabalho: string;
+  titulacao_academica: string;
+  email: string;
+  senha: string;
+}
