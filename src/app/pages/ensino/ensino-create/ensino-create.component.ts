2b93081ee3cd1c7cc38489837dc8d3d9cda25177
marksv21 <marvmms@gmail.com>
2024-04-12 13:50:50 -0300

    feat: dados mapeados de acordo com o backend

--- src/app/pages/ensino/ensino-create/ensino-create.component.ts
+++ src/app/pages/ensino/ensino-create/ensino-create.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { CrudService } from './../../../core/services/crud.service';
+import { DialogData } from './../../radoc/radoc.component';
+import { ensino, IatividadeLetiva } from './../../../core/interfaces/ensino.interface';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { FloatLabelType } from '@angular/material/form-field';
-import { ensino } from 'src/app/core/interfaces/ensino.interface';
-import { CrudService } from 'src/app/core/services/crud.service';
 
 @Component({
   selector: 'app-ensino-create',
@@ -15,107 +16,125 @@ export class EnsinoCreateComponent implements OnInit {
   @Input() isCreate!: boolean;
 
   @Input() ensino: ensino | undefined;
+  atividadeLetiva: IatividadeLetiva | undefined;
 
   @Input() ensinoSize: number | undefined;
 
   floatLabelControl = 'always' as FloatLabelType;
-
-  form!: FormGroup;
+  //forms
+  formAtividadeLetiva!: any;
+  formAtividadePedagogicaComplementar!: any;
+  formAtividadeOrientacao!: any;
+  formDescricao_orientacao!: any;
+  formsupervisao_academica!: any;
+  formPreceptoriaTutoria!: any;
+  formBanca_examinadora!: any;
+  formAvaliacao_discente!: any;
 
   step: number = 1;
-
+  atividadeNAME:any | undefined;
   ensinoId!: number;
 
-  initialForm = {
-    atividadeLetiva_codigoDisciplina: ['', [Validators.required]],
-    atividadeLetiva_nomeDisciplina: ['', [Validators.required]],
-    atividadeLetiva_ano: ['', [Validators.required]],
-    atividadeLetiva_semestre: ['', [Validators.required]],
-    atividadeLetiva_cursoNome: ['', [Validators.required]],
-    atividadeLetiva_cursoCampos_Cidade: ['', [Validators.required]],
-    atividadeLetiva_cursoCampos_Nome: ['', [Validators.required]],
-    atividadeLetiva_cursoCampos_Diretor: ['', [Validators.required]],
-    atividadeLetiva_cursoInstituto_Diretor: ['', [Validators.required]],
-    atividadeLetiva_cursoInstituto_Nome: ['', [Validators.required]],
-    atividadeLetiva_cursoInstituto_Sigla: ['', [Validators.required]],
-    atividadeLetiva_cursoNivel: ['', [Validators.required]],
-    atividadeLetiva_cursosigla: ['', [Validators.required]],
-    atividadeLetiva_docentes_envolvidos: ['', [Validators.required]],
-    atividadeLetiva_carga_horaria_docentes_envolvidos: ['', [Validators.required]],
-    atividadePedagogicaComplementar_ano: ['', [Validators.required]],
-    atividadePedagogicaComplementar_semestre: ['', [Validators.required]],
-    atividadePedagogicaComplementar_carga_horaria_semanal: ['', [Validators.required]],
-    atividadePedagogicaComplementar_docentes_envolvidos: ['', [Validators.required]],
-    atividadePedagogicaComplementar_carga_horaria_docentes_envolvidos: ['', [Validators.required]],
-    orientado_semestre: ['', [Validators.required]],
-    orientado_nome: ['', [Validators.required]],
-    orientado_matricula: ['', [Validators.required]],
-    orientado_atividade_carga_horaria: [''],
-    orientado_atividade_tipo: ['', [Validators.required]],
-    bancaExaminacao_nomeCandidato: ['', [Validators.required]],
-    bancaExaminacao_tituloTrabalho: ['', [Validators.required]],
-    bancaExaminacao_ies: ['', [Validators.required]],
-    bancaExaminacao_tipo: ['', [Validators.required]],
-    bancaExaminacao_ano: ['', [Validators.required]],
-    bancaExaminacao_semestre: ['', [Validators.required]],
-  }
+  dialogData!: DialogData | undefined;
 
   constructor(
     private readonly ensinoService: CrudService<ensino>,
-    private formBuilder: NonNullableFormBuilder,
+    private readonly formBuilder: FormBuilder,
     private _snackbar: MatSnackBar,
-  ) {
-  }
+    private atividadeLetivaService: CrudService<IatividadeLetiva>,
+    private CrudService: CrudService<any>,
+  ) {}
 
-  ngOnInit(): void {
-    this.form = this.formBuilder.group(this.initialForm);
-    if(!this.isCreate && this.ensino){
-      this.ensinoId = Number(this.ensino.id);
-      this.fillForm(this.ensino);
-    }
-  }
-
-  fillForm(ensino: ensino) {
-    this.form.patchValue({
-      atividadeLetiva_codigoDisciplina: ensino.atividade_letiva.codigo_disciplina,
-      atividadeLetiva_nomeDisciplina: ensino.atividade_letiva.nome_disciplina,
-      atividadeLetiva_ano: ensino.atividade_letiva.ano,
-      atividadeLetiva_semestre: ensino.atividade_letiva.semestre,
-      atividadeLetiva_cursoNome: ensino.atividade_letiva.curso.nome,
-      atividadeLetiva_cursoCampos_Cidade: ensino.atividade_letiva.curso.campus.cidade,
-      atividadeLetiva_cursoCampos_Nome: ensino.atividade_letiva.curso.campus.nome,
-      atividadeLetiva_cursoCampos_Diretor: ensino.atividade_letiva.curso.campus.diretor,
-      atividadeLetiva_cursoInstituto_Diretor: ensino.atividade_letiva.curso.instituto.diretor,
-      atividadeLetiva_cursoInstituto_Nome: ensino.atividade_letiva.curso.instituto.nome,
-      atividadeLetiva_cursoInstituto_Sigla: ensino.atividade_letiva.curso.instituto.sigla,
-      atividadeLetiva_cursoInstituto_Campus: ensino.atividade_letiva.curso.instituto.campus,
-      atividadeLetiva_cursoNivel: ensino.atividade_letiva.curso.nivel,
-      atividadeLetiva_cursosigla: ensino.atividade_letiva.curso.sigla,
-      atividadeLetiva_docentes_envolvidos: ensino.atividade_letiva.docentes_envolvidos,
-      atividadeLetiva_carga_horaria_docentes_envolvidos: ensino.atividade_letiva.carga_horaria_docentes_envolvidos,
-      atividadePedagogicaComplementar_ano: ensino.atividade_pedagogica_complementar.ano,
-      atividadePedagogicaComplementar_semestre: ensino.atividade_pedagogica_complementar.semestre,
-      atividadePedagogicaComplementar_carga_horaria_semanal: ensino.atividade_pedagogica_complementar.carga_horaria_semanal,
-      atividadePedagogicaComplementar_docentes_envolvidos: ensino.atividade_pedagogica_complementar.docentes_envolvidos,
-      atividadePedagogicaComplementar_carga_horaria_docentes_envolvidos: ensino.atividade_pedagogica_complementar.carga_horaria_docentes_envolvidos,
-      orientado_ano: ensino.orientado.ano,
-      orientado_semestre: ensino.orientado.semestre,
-      orientado_nome: ensino.orientado.nome,
-      orientado_matricula: ensino.orientado.matricula,
-      orientado_curso: ensino.orientado.curso,
-      orientado_tipo: ensino.orientado.tipo,
-      orientado_atividade_ano: ensino.orientado.atividade.ano,
-      orientado_atividade_semestre: ensino.orientado.atividade.semestre,
-      orientado_atividade_carga_horaria: ensino.orientado.atividade.carga_horaria,
-      orientado_atividade_tipo: ensino.orientado.atividade.tipo,
-      bancaExaminacao_nomeCandidato: ensino.banca_examinacao.nome_candidato,
-      bancaExaminacao_tituloTrabalho: ensino.banca_examinacao.titulo_trabalho,
-      bancaExaminacao_ies: ensino.banca_examinacao.ies,
-      bancaExaminacao_tipo: ensino.banca_examinacao.tipo,
-      bancaExaminacao_ano: ensino.banca_examinacao.ano,
-      bancaExaminacao_semestre: ensino.banca_examinacao.semestre,
+  async ngOnInit(){
+    this.CrudService.getAll('relatorio_docente').subscribe(result => {
+      this.atividadeNAME = result;
+    });
+    console.log(this.atividadeNAME + 'teste');
+    this.formAtividadeLetiva = this.formBuilder.group({
+      semestre:['',[Validators.required]],
+      codigo_disciplina:['',[Validators.required]],
+      nome_disciplina:['',[Validators.required]],
+      ano_e_semestre:['',[Validators.required]],
+      curso:['',[Validators.required]],
+      nivel:['',[Validators.required]],
+      numero_turmas_teorico:['',[Validators.required]],
+      numero_turmas_pratico:['',[Validators.required]],
+      ch_turmas_teorico:['',[Validators.required]],
+      ch_turmas_pratico:['',[Validators.required]],
+      docentes_envolvidos_e_cargas_horarias: this.formBuilder.group({
+        lista: this.formBuilder.array([this.createFormGroup()])
+      }), // Corrigido para utilizar array
+    });
+    this.formAtividadePedagogicaComplementar = this.formBuilder.group({
+      semestre: ['', [Validators.required]],
+      ch_semanal_graduacao: ['', [Validators.required]],
+      ch_semanal_pos_graduacao: ['', [Validators.required]],
     })
+      //atidadePedagogicaComplementar
+    this.formAtividadeOrientacao = this.formBuilder.group({
+      semestre: ['', [Validators.required]],
+      ch_semanal_orientacao: ['', [Validators.required]],
+      ch_semanal_coorientacao: ['', [Validators.required]],
+      ch_semanal_supervisao: [''],
+      ch_semanal_preceptoria_e_ou_tutoria: ['', [Validators.required]],
+    })
+    //Descrição Orientacao
+    this.formDescricao_orientacao = this.formBuilder.group({
+      numero_doc: ['', [Validators.required]],
+      nome_e_ou_matricula_discente: ['', [Validators.required]],
+      curso: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      nivel: ['', [Validators.required]],
+      ch_semanal_primeiro_semestre: ['', [Validators.required]],
+      ch_semanal_segundo_semestre: ['', [Validators.required]],
+    })
+    //SupervisaoAcademica
+    this.formsupervisao_academica = this.formBuilder.group({
+      numero_doc: ['', [Validators.required]],
+      nome_e_ou_matricula_discente: ['', [Validators.required]],
+      curso: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      nivel: ['', [Validators.required]],
+      ch_semanal_primeiro_semestre: ['', [Validators.required]],
+      ch_semanal_segundo_semestre: ['', [Validators.required]],
+    })
+    //PreceptoriaTutoria
+    this.formPreceptoriaTutoria = this.formBuilder.group({
+      numero_doc: ['', [Validators.required]],
+      nome_e_ou_matricula_discente: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      ch_semanal_primeiro_semestre: ['', [Validators.required]],
+      ch_semanal_segundo_semestre: ['', [Validators.required]],
+    })
+    //BancaExaminadora
+    this.formBanca_examinadora = this.formBuilder.group({
+      numero_doc: ['', [Validators.required]],
+      nome_candidato: ['', [Validators.required]],
+      titulo_trabalho: ['', [Validators.required]],
+      ies: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      ch_semanal_primeiro_semestre: ['', [Validators.required]],
+      ch_semanal_segundo_semestre: ['', [Validators.required]],
+    })
+    //AvaliacaoDiscente
+    this.formAvaliacao_discente = this.formBuilder.group({
+      numero_doc_primeiro_semestre: ['', [Validators.required]],
+      nota_primeiro_semestre: ['', [Validators.required]],
+      codigo_turma_primeiro_semestre: ['', [Validators.required]],
+      numero_doc_segundo_semestre: ['', [Validators.required]],
+      nota_segundo_semestre: ['', [Validators.required]],
+      codigo_turma_segundo_semestre: ['', [Validators.required]],
+    })
+
+    if(!this.isCreate){
+      this.ensinoId = Number(this.ensino?.id);
+    }
+    console.log(this.ensino)
+    console.log(this.atividadeLetiva)
+    console.log(this.formBuilder.array([this.formBuilder.control('')]));
+
   }
+
 
   goBack() {
     if (this.step > 1) {
@@ -124,14 +143,14 @@ export class EnsinoCreateComponent implements OnInit {
   }
 
   goNext() {
-    if (this.step < 5) {
+    if (this.step < 8) {
       this.step++;
     }
   }
 
   async submit() {
-    const formValue = this.form.getRawValue();
-    if (!this.form.valid) {
+    const formValue = this.formAtividadeLetiva.getRawValue();
+    if (!this.formAtividadeLetiva.valid) {
       this._snackbar.open('Preencha todos os campos.', 'OK', {
         duration: 5000
       });
@@ -142,16 +161,64 @@ export class EnsinoCreateComponent implements OnInit {
       this.isCreate ?
         await this.ensinoService.create('ensino',formValue) :
         await this.ensinoService.update('ensino', formValue);
-        this._snackbar.open('Relátorio de ensino salvo com sucesso.', 'OK', {
+        this._snackbar.open('Relatório de ensino salvo com sucesso.', 'OK', {
           duration: 5000
         });
-      location.reload();
+      // Evitando o uso de location.reload() para atualizar a interface do usuário
     } catch (error) {
       console.error(error);
-      this._snackbar.open('Erro ao salvar Relátorio de ensino.', 'OK', {
+      this._snackbar.open('Erro ao salvar Relatório de ensino.', 'OK', {
         duration: 5000
       });
     }
   }
 
+  createFormGroup() {
+    return this.formBuilder.group({
+      nome_docente: ['',[Validators.required]],
+      carga_horaria: ['',[Validators.required]],
+    });
+  }
+  addlista() {
+    this.lista.push(this.createFormGroup())
+    console.log(this.formAtividadeLetiva.value);
+  }
+  get lista(): FormArray {
+    return this.formAtividadeLetiva.get('docentes_envolvidos_e_cargas_horarias.lista') as FormArray;
+  }
+
+  removerDocente(index: number): void {
+    this.lista.removeAt(index);
+  }
+  async submitAtividadeLetiva() {
+    const formValue = this.formAtividadeLetiva.getRawValue();
+    console.log(formValue);
+
+    if (!this.formAtividadeLetiva.valid) {
+      this._snackbar.open('Preencha todos os campos.', 'OK', {
+        duration: 5000
+      });
+      return;
+    }
+
+    try {
+      if (this.isCreate) {
+        this.atividadeLetivaService.createEnsino('atividade_letiva', formValue, 'teste');
+        this._snackbar.open('Relatório de atividade letiva criado com sucesso.', 'OK', {
+          duration: 5000
+        });
+      } else {
+        await this.atividadeLetivaService.update('atividade_letiva', formValue);
+        this._snackbar.open('Relatório de atividade letiva atualizado com sucesso.', 'OK', {
+          duration: 5000
+        });
+      }
+      // Aqui você pode atualizar a interface do usuário sem recarregar a página
+    } catch (error) {
+      console.error(error);
+      this._snackbar.open(error as string, 'OK', {
+        duration: 5000
+      });
+    }
+  }
 }
