52f7834cd5c7e861d776dab5f0f1e14211f08639
marksv21 <marvmms@gmail.com>
2024-04-16 16:49:24 -0300

    fix: login autenticação e configurações

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,7 +7,7 @@ import { AutenticacaoService } from 'src/app/core/services/autenticacao.service'
 import { TokenService } from 'src/app/core/services/token.service';
 
 export interface Login {
-  email: string;
+  login: string;
   password: string;
 }
 @Component({
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
   isLogged: boolean = false;
   floatLabelControl = 'always' as FloatLabelType;
   initialForm = {
-    username: ['', [Validators.required, Validators.email]],
+    login: ['', [Validators.required]],
     password: ['', [Validators.required,Validators.minLength(3)]],
   }
 
@@ -37,14 +37,14 @@ export class LoginComponent implements OnInit {
 
   fillForm(usuario: Login ) {
     this.form.patchValue({
-      username: usuario.email,
+      login: usuario.login,
       password: usuario.password
     });
   }
 
   login(){
     if(this.form.valid) {
-      const email = this.form.value.username || this.form.value.email;
+      const email = this.form.value.login || this.form.value.email;
       const senha = this.form.value.password;
       console.log(this.form.value);
 
@@ -61,7 +61,7 @@ export class LoginComponent implements OnInit {
           this._snackbar.open('Seu e-mail/username ou senha inválidos', 'OK', {
             duration: 5000
           })
-          console.log('error:' + err)
+          console.log('error:' + err.message);
         },
       })
     }
