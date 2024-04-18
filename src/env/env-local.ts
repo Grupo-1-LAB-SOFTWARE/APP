946134b29c20bcb75d4702bc8280100da1e54a0e
Amanda Lima Reis <amandalreis@me.com>
2024-04-18 16:06:08 -0300

    prod

--- src/env/env-local.ts
+++ src/env/env-local.ts
@@ -1,5 +1,7 @@
 const API_HOST = 'http://127.0.0.1';
 // const API_PATH = 'api';
 const API_PORT = '8000';
+const API_PROD = 'https://api-sisradoc.up.railway.app';
 
-export const API_URL = `${API_HOST}:${API_PORT}`;
+//export const API_URL = `${API_HOST}:${API_PORT}`;
+export const API_URL = `${API_PROD}`;
